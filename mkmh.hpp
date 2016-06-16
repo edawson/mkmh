6e22726a812a88585676f99ed38ab6a081194e1f
Eric T Dawson <eric.t.dawson@gmail.com>
2016-06-16 17:02:06 +0100

    Added sigs for multi functions

--- mkmh.hpp
+++ mkmh.hpp
@@ -2,8 +2,13 @@
 
 /* Returns the forward and reverse-reverse complement kmers of a sequence */
 vector<string> kmerize(string seq, int k);
+
+vector<string> multi_kmerize(string seq, vector<int> k);
+
 /* Returns the forward shingles size k of a sequence */
 vector<string> shingle(string seq, int k);
+
+vector<string> multi_shingle(string seq, vector<int> k);
 
 vector<int64_t> top_minhash_64(string seq, int k, int hashSize);
 
